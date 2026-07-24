e94e0143bbe4d19de2fd01a2e3fdaf1ed124020f
parsa <parsapursmaili@gmail.com>
2026-07-24 15:09:09 +0330

    fix(map-test): migrate consolidation engine to deterministic dictionary mapping and targeted island size filters

--- src/engine/world-divider/world-consolidator.ts
+++ src/engine/world-divider/world-consolidator.ts
@@ -1,21 +1,12 @@
 import { InputFeature } from "./types";
-import { calculatePolygonArea, getDistance } from "./geometry-utils";
+import { calculatePolygonArea } from "./geometry-utils";
 import { PolygonDissolver } from "@/engine/map/utils/polygon-dissolver";
 import { DELETED_COUNTRIES } from "@/application/config/deleted-countries";
+import { MINOR_MERGE_MAP } from "@/application/config/minor-merge-map";
 
 export function consolidateWorldMap(features: InputFeature[]): InputFeature[] {
-  const countries: {
-    code: string;
-    name: string;
-    polygons: [number, number][][];
-    area: number;
-    edges: {
-      p1: [number, number];
-      p2: [number, number];
-      mid: [number, number];
-    }[];
-    neighbors: Set<string>;
-  }[] = [];
+  const countryPolygons = new Map<string, [number, number][][]>();
+  const countryNames = new Map<string, string>();
 
   features.forEach((f) => {
     const rawCode =
@@ -45,125 +36,52 @@ export function consolidateWorldMap(features: InputFeature[]): InputFeature[] {
       });
     }
 
+    const minAreaLimit = code === "CAN" || code === "RUS" ? 12.0 : 0.005;
     const filteredPolygons: [number, number][][] = [];
-    let totalArea = 0;
-
-    const minAreaLimit = code === "CAN" || code === "RUS" ? 12.0 : 0.1;
 
     rawPolygons.forEach((poly) => {
       const area = calculatePolygonArea(poly);
       if (area >= minAreaLimit) {
         filteredPolygons.push(poly);
-        totalArea += area;
       }
     });
 
     if (filteredPolygons.length === 0) return;
 
-    const edges: {
-      p1: [number, number];
-      p2: [number, number];
-      mid: [number, number];
-    }[] = [];
-    filteredPolygons.forEach((poly) => {
-      for (let i = 0; i < poly.length; i++) {
-        const p1 = poly[i];
-        const p2 = poly[(i + 1) % poly.length];
-        if (p1 && p2) {
-          edges.push({
-            p1,
-            p2,
-            mid: [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2],
-          });
-        }
-      }
-    });
-
-    countries.push({
-      code,
-      name,
-      polygons: filteredPolygons,
-      area: totalArea,
-      edges,
-      neighbors: new Set<string>(),
-    });
-  });
-
-  for (let i = 0; i < countries.length; i++) {
-    for (let j = i + 1; j < countries.length; j++) {
-      const c1 = countries[i]!;
-      const c2 = countries[j]!;
-
-      let isAdjacent = false;
-      for (const e1 of c1.edges) {
-        for (const e2 of c2.edges) {
-          if (getDistance(e1.mid, e2.mid) < 0.05) {
-            isAdjacent = true;
-            break;
-          }
-        }
-        if (isAdjacent) break;
-      }
-
-      if (isAdjacent) {
-        c1.neighbors.add(c2.code);
-        c2.neighbors.add(c1.code);
+    let targetCode = code;
+    if (DELETED_COUNTRIES.has(code)) {
+      const mappedTarget = MINOR_MERGE_MAP[code];
+      if (mappedTarget) {
+        targetCode = mappedTarget;
+      } else {
+        return;
       }
     }
-  }
 
-  const activeCountries = countries.filter(
-    (c) => !DELETED_COUNTRIES.has(c.code),
-  );
-  const deletedCountries = countries.filter((c) =>
-    DELETED_COUNTRIES.has(c.code),
-  );
-
-  deletedCountries.sort((a, b) => a.area - b.area);
-
-  deletedCountries.forEach((c) => {
-    let smallestNeighbor: typeof c | undefined;
-    let minArea = Infinity;
-
-    c.neighbors.forEach((nCode) => {
-      const neighbor = countries.find((x) => x.code === nCode);
-      if (neighbor && neighbor.polygons.length > 0 && neighbor.area < minArea) {
-        minArea = neighbor.area;
-        smallestNeighbor = neighbor;
-      }
-    });
-
-    if (smallestNeighbor) {
-      smallestNeighbor.polygons.push(...c.polygons);
-      smallestNeighbor.area += c.area;
-      c.neighbors.forEach((nCode) => {
-        if (nCode !== smallestNeighbor!.code) {
-          smallestNeighbor!.neighbors.add(nCode);
-        }
-      });
-      c.polygons = [];
-      c.area = 0;
-    } else {
-      c.polygons = [];
-      c.area = 0;
+    if (!countryPolygons.has(targetCode)) {
+      countryPolygons.set(targetCode, []);
+      countryNames.set(targetCode, name);
     }
+
+    countryPolygons.get(targetCode)!.push(...filteredPolygons);
   });
 
   const dissolver = new PolygonDissolver();
   const resultFeatures: InputFeature[] = [];
 
-  activeCountries.forEach((c) => {
-    if (c.polygons.length === 0) return;
-    const dissolved = dissolver.dissolve(c.polygons);
+  countryPolygons.forEach((polys, code) => {
+    if (polys.length === 0) return;
+    const name = countryNames.get(code) || code;
+    const dissolved = dissolver.dissolve(polys);
 
     resultFeatures.push({
       type: "Feature",
       properties: {
-        adm0_a3: c.code,
-        ISO_A3: c.code,
-        iso_a3: c.code,
-        name: c.name,
-        NAME: c.name,
+        adm0_a3: code,
+        ISO_A3: code,
+        iso_a3: code,
+        name,
+        NAME: name,
       },
       geometry: {
         type: "MultiPolygon",
