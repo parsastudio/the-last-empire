7a38722898521a5de9c1a9f32eb9e5a0415cbccd
parsa <parsapursmaili@gmail.com>
2026-07-28 14:06:04 +0330

    refactor(cleanup): remove all unit test suites and redundant debug API endpoints

--- src/engine/combat/tests/militia-garrison.test.ts
+++
@@ -1,9 +0,0 @@
-import { HomelandMilitiaCalculator } from "../math/homeland-militia-calculator";
-
-export function runMilitiaGarrisonTest(): boolean {
-  const calculator = new HomelandMilitiaCalculator();
-
-  const militiaPower = calculator.calculateMilitiaGarrisonPower(36000000, 80);
-
-  return militiaPower === 288;
-}
