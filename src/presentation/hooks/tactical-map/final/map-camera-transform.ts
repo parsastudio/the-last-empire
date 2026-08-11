b7983c9d14e80b86c6f43b35e9d670248fc34265
parsa <parsapursmaili@gmail.com>
2026-08-11 14:55:30 +0330

    refactor(cleanup): execute phase 1 - remove dead files and unused dependencies

--- src/presentation/hooks/tactical-map/final/map-camera-transform.ts
+++ src/presentation/hooks/tactical-map/final/map-camera-transform.ts
@@ -1,13 +1,4 @@
-import { RefObject } from "react";
-
 export interface CameraPosition {
   x: number;
   y: number;
-}
-
-export interface CameraTransformRef {
-  positionRef: RefObject<CameraPosition>;
-  scaleRef: RefObject<number>;
-  isDraggingRef: RefObject<boolean>;
-  hasDraggedRef: RefObject<boolean>;
 }
